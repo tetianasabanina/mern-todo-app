7dd4852629842ffd4de34ad372976ac441cb7b34
Sabanina Tetiana <tatiana08st@gmail.com>
2019-11-02 20:52:11 +0200

    Updates

---
+++ precache-manifest.71674434ebee62ffe06d0f806a0abf5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955819ebda35c4b204ebdfaf351d642b",
+    "url": "%7Btetianasabanina%7D.github.io/%7Bmern-todo-app%7D/index.html"
+  },
+  {
+    "revision": "3cb59d8a81316ee0553c",
+    "url": "%7Btetianasabanina%7D.github.io/%7Bmern-todo-app%7D/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9c224f2971a54f8880fc",
+    "url": "%7Btetianasabanina%7D.github.io/%7Bmern-todo-app%7D/static/css/main.d9d7de69.chunk.css"
+  },
+  {
+    "revision": "3cb59d8a81316ee0553c",
+    "url": "%7Btetianasabanina%7D.github.io/%7Bmern-todo-app%7D/static/js/2.5fa7a13b.chunk.js"
+  },
+  {
+    "revision": "9c224f2971a54f8880fc",
+    "url": "%7Btetianasabanina%7D.github.io/%7Bmern-todo-app%7D/static/js/main.790c7b29.chunk.js"
+  },
+  {
+    "revision": "4983333e7b0f07d30d81",
+    "url": "%7Btetianasabanina%7D.github.io/%7Bmern-todo-app%7D/static/js/runtime-main.096de1ea.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "%7Btetianasabanina%7D.github.io/%7Bmern-todo-app%7D/static/media/logo.25bf045c.svg"
+  }
+]);
